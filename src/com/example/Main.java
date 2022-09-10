7958be6b7e26e26330167e10cb99822b36102ae9
Joel Povis <joel.povis@seidor.com>
2022-09-09 23:14:28 -0500

    Add the file Main.java

---
+++ src/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+/**
+ * @author <a href="mailto:jpovis@seidor.es">Joel Povis</a>
+ */
+public class Main {
+    public static void main(String... arg) {
+        System.out.println("Hola mundo!");
+    }
+}
